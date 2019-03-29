cae6cc4e92526eb6b2330bfb467fea2fe7c9f97c
MisterMoody <principal@mistermoody.com>
2019-03-29 12:01:51 -0400

    Add Homepage

---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+    <title>Mister Moody</title>
+    <meta name='description' content="Single-Page Portfolio" />
+    <meta name="keywords" content="" />
+    <!-- Browser Tab Icon -->
+    <link rel="shortcut icon" href="img/me.jpg" type="img/m" class="rounded">    
+    <!-- CSS Styles -->   
+    <link rel="preload" href="main.css" as="style">
+    <link rel="stylesheet" href="css/minified.css">
+    <!-- Custom Fonts -->       
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bree+Serif|Raleway">
+    <!-- FontAwesome Fonts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Treehouse 'Badges' ReportCard by Riley Hilliard -->
+    <link rel="preload" href="js/reportcard/js/reportcard.js" as="script">
+    <link rel="preload" href="js/reportcard/css/badges.css" as="style">
+    <link href="js/reportcard/css/badges.css" rel="stylesheet">
+  </head>
+  <body>
+  <!-- ********************************************************* -->
+  <!-- ********************************************************* -->
+    <!-- ****** <MAIN-GRID>  ***** -->
+      <!-- //CONTENT-BEGINS..... -->
+
+    <!-- ****** <HOMEPAGE>  ***** -->
+    <article class="indexGrid">    
+      
+      <!-- //HEADER (Logo & Navigation) -->
+      <header>
+        <div class="header__logo"><h1>Mister Moody</h1></div>
+          <ul class="header__title">
+            <li><a href="#about">About</a></li>
+            <li><a href="#skills">Training</a></li>
+            <li><a href="#work">Experience</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+      </header> 
+      
+      <!-- //CONTENT -->
+      <main>
+        <!-- #About (Introduction) -->
+        <section>
+          <p>Non-traditional educator with a plethora of experiences delivering language acquisition services for private and public institutions of education in China, Japan, South Korea and the United States.</p>
+          <picture><img src="img/MMgrey.jpg" class="mainImage" alt="Mister Moody Logo"/></picture>
+          <p id="about">Self-taught web implementation specialist able to proficiently design, develop and deploy a web application to specification.</p>
+          <p><img src="img/tailorVanilla.jpg" class="imgL" alt="Sewing in a Chinese Factory"/>As a designer, I can adapt a low-fidelity prototype into a viable website using HTML, CSS, JavaScript, content management tools and digital media. As a developer, I utilize the PHP scripting language that incorporates object-oriented programming practices to integrate server-side functionality into a web application. Vital components of this process entails auscultating user-experience; amalgamating accessibility, performance requirements and other technical considerations; HTTP server installation; database configuration; and using the SSH protocol in conjunction with version control to provide secure remote access to mitigate maintenance concerns. To this end, I am currently exploring opportunities by which I can leverage my skillset to pursue a position in the tech industry.</p>
+        </section>
+        <!-- -->
+        <br><hr style="margin: 0 20px;"><br>
+        <!-- #Training (Skills) -->
+        <section id="skills">
+          <h4>Training &amp; Skills</h4>
+          <p>Jack-of-All-Trades, I am not. However, I am an effective communicator with a distinctive international experience capable of doing anything that I put my mind to.</p>
+          <div class="subGrid">
+            <!-- Training -->
+            <div>
+              <div class="grid-table">
+                <span>2015</span>
+                <span>Web Dev Training</span>
+                <span>USA</span>
+                <span>Treehouse</span>
+
+                <span>2011</span>
+                <span>Mandarin Language Studies</span>
+                <span>China</span>
+                <span>Shanghai Ocean University</span>
+
+                <span>2010</span>
+                <span>MOS Training</span>
+                <span>USA</span>
+                <span>Microsoft</span>
+
+                <span>2007</span>
+                <span>TEFL Teacher Training</span>
+                <span>Thailand</span>
+                <span class="smallSpan">International TEFL Teacher Training, Inc.</span>
+
+                <span>2003</span>
+                <span>B.A. Political Science</span>
+                <span>USA</span>
+                <span>University of Louisville</span>
+              </div>
+            </div>
+            <!-- Skills -->
+            <div>
+              <div class="grid-row">
+                <div class="SkillBar">
+                  <div id="Skill-Design">
+                    <span class="Skill-Area">Design</span> <span class="PercentText">80%</span>
+                  </div>
+                </div>
+                <div class="skillSpecs">
+                  <span>HTML5 | CSS3 | IA/UI/UX | Bootstrap</span>
+                </div>
+              </div>
+              <div class="grid-row">
+                <div class="SkillBar">
+                  <div id="Skill-Develop">
+                    <span class="Skill-Area">Develop</span> <span class="PercentText">68%</span>
+                  </div>
+                </div>
+                <div class="skillSpecs">
+                  <span>PHP7 | Composer | JavaScript | npm</span>
+                </div>
+                </div>
+              <div class="grid-row">
+                <div class="SkillBar">
+                  <div id="Skill-Deploy">
+                    <span class="Skill-Area">Deploy</span> <span class="PercentText">82%</span>
+                  </div>
+                </div>
+                <div class="skillSpecs">
+                  <span>Server Config. | Version Control | SFTP | SSH</span>
+                </div>
+              </div>
+              <div class="grid-row">
+                <div class="SkillBar">
+                  <div id="Skill-Copy">
+                    <span class="Skill-Area">Writing &amp; Editing</span> <span class="PercentText">91%</span>
+                  </div>
+                </div>
+                <div class="skillSpecs">
+                  <span>Creative | AP &amp; MLA Style | Research</span>
+                </div>
+              </div>
+              <div class="grid-row">
+                <div class="SkillBar">
+                  <div id="Skill-Interpersonal">
+                    <span class="Skill-Area">Interpersonal</span> <span class="PercentText">79%</span>
+                  </div>
+                </div>
+                <div class="skillSpecs">
+                  <span>Articulate | Ethical | Problem Solver | Weird A.F.</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- -->
+        <br><hr style="margin: 0 20px;"><br>
+        <!-- #Experience (Work) -->
+        <section id="work">
+          <div>
+            <h4>WORK Section</h4>
+            <p>I am currently employed as a substitute teacher with Jefferson County Public Schools. In my spare time, I study web technologies in an effort to develop my sole-proprietorship!</p>
+            <!-- Resume -->
+            <details><!-- <details 'open'> will OPEN Module from Start -->
+              <summary>Teaching Experience &nbsp;<code>9,000+ Hours</code></summary>
+              <p>Below is a list of some of the educational institutions that I have collaborated with:</p>
+              <div class="trainingGrid">
+                <span>2018 - <!--<small><i>Present</i></small>--> </span>
+                <span><!--Substitute Teacher <b>&#64;</b> -->Jefferson Co. Public Schools <small>(KY)</small></span>
+                <span>2016 - 2017 </span>
+                <span><!--TEFL Instructor <b>&#64;</b> -->Jiaxing College</span>
+                <span>2012 - 2013 </span>
+                <span><!--TEFL Instructor <b>&#64;</b> -->Zhejiang Middle School</span>
+                <span>2007 - 2009 </span>
+                <span><!--TEFL Trainer <b>&#64;</b> -->Oxford Seminars</span>
+                <span>2005 - 2006 </span>
+                <span><!--TEFL Instructor <b>&#64;</b> -->Joongdong High School</span>
+                <span>2004 - 2006 </span>
+                <span><!--TEFL Consultant <b>&#64;</b> -->US-Japan Edu. Exchange Program</span>
+                <span>2003 - 2004 </span>
+                <span><!--TEFL Instructor <b>&#64;</b> -->Sogang University</span>
+                <span>2000 - 2001 </span>
+                <span><!--Rec. Supervisor <b>&#64;</b> -->Cabbage Patch Settlement House</span>
+              </div> 
+            </details>
+            <!-- Treehouse Marks -->
+            <details>
+              <summary>Web Dev Experience &nbsp;<code>2,000+ Hours</code></summary>
+              <div class="report-card treehouse"></div>
+            </details>
+          </div>
+        </section>
+        <!-- -->
+        <br><hr style="margin: 0;"><br>
+        <!-- #Contact -->
+        <section id="contact">
+          <h4>Get In Touch</h4>
+          <p class="readText">Are you an employer with a specific opportunity that you would like to share? A small business seeking an overhaul of your legacy website? An entrepreneur in need of a digital presence?  A student seeking mentorship? No matter your situation, I offer services to all!</p>
+          <ul class="social">
+            <li>
+              <a class="github" href="https://github.com/MisterMoody" target="_blank" title="Check Out My Work on Github">
+              <i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
+            </li>
+            <li>
+              <a class="twitter" href="https://twitter.com/Mister_Moody" target="_blank" title="Follow Me on Twitter">
+              <i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+            </li>
+            <li>
+              <a class="download" href="moodyCV.doc" download="moodyCV.doc" title="Download Resume">
+              <i class="fa fa-download fa-2x" aria-hidden="true"></i></a>
+            </li>
+            <li>
+              <a class="phone" href="tel:+15025589240" title="Call Me">
+              <i class="fa fa-mobile fa-2x" aria-hidden="true"></i></a>
+            </li>
+            <li>
+              <a class="email" href="mailto:raymoody10@gmail.com" title="Email Me">
+              <i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </section>
+        <!-- -->
+      </main>
+      
+      <!-- //FOOTER --> 
+      <footer>
+        <small>&#169;<span><?php echo date('Y'); ?></span></small>&#32;&#32;
+        Ray Moody dba Mister Moody
+      </footer> 
+      
+    </article>
+    <!-- ************************* -->
+
+
+    <!-- JAVASCRIPT ************** --> 
+    <!-- \\\\\\ Font-Awesome Icons -->
+    <script src="https://use.fontawesome.com/ceeedfe13b.js"></script>
+    <!-- \\\\\\ Reportcard JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="js/reportcard/js/reportcard.js"></script>
+    <script>$('.report-card.treehouse').reportCard({
+            userName: 'mistermoody',
+            site: 'treehouse',
+            badgesAmount: 7});</script>
+    <!-- ************************* --> 
+  </body>
+</html>
